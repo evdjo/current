73016ffa7fe3971775e95a5e45235f8fc1432fe2
evdjo <evdjo@abv.bg>
2014-03-22 01:03:17 +0000

    renamed.

---
+++ cs225_c/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: evdjoint
+ *
+ * Created on 22 March 2014, 00:59
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
